2cd56af8c5c7fd43ffe62a334fd55d5a90791566
suzuka1621 <akaneakua0625@gmail.com>
2023-05-15 22:17:34 +0900

    PHPの定数

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+//PHP書き方
 
 /*数字 と 文字
 数字は半角
@@ -19,10 +19,18 @@ echo('<br>');
 echo('こちらはPHPです');
 echo('<br>');
 
- ?>
+?>
+
+
+
+
+
+
 
 <?php
-// 変数 PHPは 動的型付 
+//PHP変数
+
+// PHPは 動的型付 
 //Java, Visual Basicは静的型付
 
 // integer・・数字
@@ -39,7 +47,28 @@ $test_3 = $test_1 . $test_2;
 //$test = 'テストです';
 
 // 配列 オブジェクト コレクション型
+echo('<br>');
 Var_dump($test_3);
 echo('<br>');
 //echo $test;
+?>
+
+
+
+
+
+
+
+
+
+
+<?php
+//PHP定数
+
+//定数 変わらない数・文字
+// constantの略
+const MAX = 'テスト';
+echo('<br>');
+echo MAX;
+
 ?>
