8e38ddbfc01888bcc646694f17aa96dd061e9761
suzuka1621 <akaneakua0625@gmail.com>
2023-05-15 21:43:35 +0900

    PHPの変数

--- index.php
+++ index.php
@@ -17,5 +17,22 @@ echo('<br>');
 echo('こんば""んは');
 echo('<br>');
 echo('こちらはPHPです');
+echo('<br>');
 
  ?>
+
+<?php
+// 変数 動的型付 Java, Visual Basic 静的型付
+
+// integer・・数字
+// string・・文字
+
+$test = 123;
+//$test = 'テストです';
+
+// 配列 オブジェクト コレクション型
+Var_dump($test);
+echo('<br>');
+//echo $test;
+?>
+
