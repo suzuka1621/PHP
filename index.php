9fbdd23c26850bc2ff0b78ded5b74745a4610b98
suzuka1621 <akaneakua0625@gmail.com>
2023-05-16 23:23:26 +0900

    and,or,三項演算子

--- index.php
+++ index.php
@@ -218,19 +218,53 @@ echo('<br>');
   //条件が偽なら実行
 //}
 
-$height = 90;
+$height = 91;
 
 echo('<br>');
 var_dump($height);
 
-echo('<br>');
-if ($height >= 91){ //条件は数字
-  echo '身長は' . $height . 'cmです';
-} 
+// echo('<br>');
+// if ($height >= 91){ //条件は数字
+//   echo '身長は' . $height . 'cmです';
+// } 
 
-if ($height <= 90){ 
-  echo '身長は' . $height . 'cmになります。';
+// if ($height <= 90){ 
+//   echo '身長は' . $height . 'cmになります。';
+// }
+
+echo('<br>');
+if ($height !== 90){ //型が同じかどうかも判定 
+  echo '身長は90cmではありません。';
 }
+
+//データが入っているかどうか
+// isset empty is_null
+echo('<br>');
+$test = 1; //文字
+
+if(!empty($test)){
+  echo '変数は空ではありません';
+}
+
+// AND と OR
+$signal_1 = 'red';
+$signal_2 = 'yellow';
+
+echo('<br>');
+if ($signal_1 === 'red' || $signal_2 === 'blue'){
+  echo '赤です';
+}
+
+
+//三項演算子
+//if else
+//条件 ? 真 : 偽
+
+$math = 80;
+echo('<br>');
+$comment = $math > 80 ? 'good' : 'not good';
+
+echo $comment;
 
 
 //elseを使う場合
