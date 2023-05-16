0d617378e694c35eb7b6b42f9224c281b8fadc89
suzuka1621 <akaneakua0625@gmail.com>
2023-05-16 23:43:54 +0900

    foreachの使い方

--- index.php
+++ index.php
@@ -130,7 +130,7 @@ var_dump($array_member);
 echo '</pre>';
 
 
-// 階層構造
+// 多段階
 $array_member_2 = [
   '本田' => [
     'height' => 170,
@@ -288,9 +288,61 @@ if ($signal === 'blue'){
     echo 'スピード違反';
   }
 }
-
+echo('<br>');
 
 // == 一致
 // === 型も一致するか判定する ・string ・integerなど
+
+?>
+
+
+
+
+
+
+
+<?php
+// foreach
+// 複数の値 foreach
+
+$members = [
+  'name' => '本田',
+  'height' => 170,
+  'hobby' => 'サッカー'
+];
+
+// バリューのみ表示
+foreach($members as $member){
+  echo $member;
+}
+
+  echo '<br>';
+// キーとバリューそれぞれ表示
+foreach($members as $key => $value){
+  echo $key . 'は' . $value . 'です';
+}
+
+echo '<br>';
+
+
+//多段階の配列を展開
+$members_2 = [
+  '本田' => [
+  'height' => 170,
+  'hobby' => 'サッカー'
+  ],
+  '香川' => [
+  'height' => 165,
+  'hobby' => 'サッカー'
+  ]
+];
+
+foreach($members_2 as $member_1){
+  foreach($member_1 as $member => $value){
+  echo $member . 'は' . $value . 'です';
+  }
+}
+echo '<br>';
+
 
 ?>
